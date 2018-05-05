31488328507fa6651e1874dfa1a756ea287c726e
Zac Garby <me@zacgarby.co.uk>
2018-05-05 13:53:46 +0100

    Initial Commit

---
+++ Tab Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tab Viewer
+//
+//  Created by Zac Garby on 05/05/2018.
+//  Copyright © 2018 Zac Garby. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
